dcc9c411e0a7ba47140015df7a5c1b6095b69560
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 22:23:03 +0200

    Add class AnimalType

--- Animals/src/myVision/AnimalType.java
+++ Animals/src/myVision/AnimalType.java
@@ -1,2 +1,21 @@
-package myVision;public class AnimalType {
+package myVision;
+
+public enum AnimalType {
+
+    Cat,
+    Dog,
+    Hamster;
+
+    public static AnimalType getType (int typeId){
+        switch (typeId){
+            case 1:
+                return AnimalType.Cat;
+            case 2:
+                return AnimalType.Dog;
+            case 3:
+                return AnimalType.Hamster;
+            default:
+                return null;
+        }
+    }
 }
