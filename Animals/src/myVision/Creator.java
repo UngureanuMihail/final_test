130dc5ec75b9587724eb4dd1dfb0362ce8d684ea
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 22:23:39 +0200

    Add class Creator

--- Animals/src/myVision/Creator.java
+++ Animals/src/myVision/Creator.java
@@ -1,2 +1,16 @@
-package myVision;public class Creator {
+package myVision;
+
+import java.time.LocalDate;
+
+public abstract class Creator {
+
+    protected abstract Animal createNewPet(AnimalType type);
+
+    public Animal createPet(AnimalType type, String name, LocalDate date) {
+
+        Animal pet = createNewPet(type);
+        pet.setName(name);
+        Animal.setBirthday(date);
+        return pet;
+    }
 }
