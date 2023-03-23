78c37254f4248434962713fb42a9daee2a4196a6
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 22:56:33 +0200

    Add class View

--- Animals/src/Interface/View.java
+++ Animals/src/Interface/View.java
@@ -1,2 +1,12 @@
-package Interface;public class View {
+package Interface;
+
+import java.util.List;
+
+public interface View <T>{
+
+    String getName();
+    String getBirthday();
+    <U> void printAll (List <U> list, Class <U> clazz);
+    void showMessage (String s);
+
 }
