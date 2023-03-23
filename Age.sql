80a5231ca096adef7e8eb8c89bdf36e82c0619ca
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 21:47:19 +0200

    Сортировка по воздрасту

---
+++ Age.sql
@@ -0,0 +1,12 @@
+CREATE TEMPORARY TABLE animals AS 
+SELECT *, 'Лошади' as genus FROM horses
+UNION SELECT *, 'Ослы' AS genus FROM donkeys
+UNION SELECT *, 'Собаки' AS genus FROM dogs
+UNION SELECT *, 'Кошки' AS genus FROM cats
+UNION SELECT *, 'Хомяки' AS genus FROM hamsters;
+
+CREATE TABLE yang_animal AS
+SELECT Name, Birthday, Commands, genus, TIMESTAMPDIFF(MONTH, Birthday, CURDATE()) AS Age_in_month
+FROM animals WHERE Birthday BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR) AND ADDDATE(CURDATE(), INTERVAL -1 YEAR);
+ 
+SELECT * FROM yang_animal;
