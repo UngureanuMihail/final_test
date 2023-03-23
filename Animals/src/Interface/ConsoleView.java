c9f3c0245b20f908d1864dbf35cb596841015380
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 23:00:09 +0200

    Работа с меню

--- Animals/src/Interface/ConsoleView.java
+++ Animals/src/Interface/ConsoleView.java
@@ -1,2 +1,45 @@
-package Interface;public class ConsoleView {
+package Interface;
+
+import java.util.List;
+import java.util.Scanner;
+import myVision.*;
+
+public class ConsoleView implements View <Animal> {
+
+    Scanner in;
+
+    public ConsoleView() {
+        in = new Scanner(System.in, "ibm866");
+    }
+
+    @Override
+    public String getName() {
+        System.out.printf("Имя: ");
+        return in.nextLine();
+    }
+
+    @Override
+    public String getBirthday() {
+        System.out.printf("Введите дату рождения в формате 'dd.mm.yyyy': ");
+        return in.nextLine();
+    }
+
+    @Override
+    public <T> void printAll (List <T> list, Class <T> clazz) {
+        System.out.print("\033[H\033[J");
+        if (list.isEmpty())
+            System.out.println("Записи не найдены");
+        else {
+            if (clazz == Animal.class)
+                System.out.println("\n          Наши питомцы:");
+            for (T item : list) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    @Override
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
 }
