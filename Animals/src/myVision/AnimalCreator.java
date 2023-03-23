266a7e671866a0c183a02ed4563c606f2aa2c274
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 22:21:52 +0200

    Add class AnimalCretor

--- Animals/src/myVision/AnimalCreator.java
+++ Animals/src/myVision/AnimalCreator.java
@@ -1,2 +1,18 @@
-package myVision;public class AnimalCreator {
+package myVision;
+
+public class AnimalCreator extends Creator {
+
+    @Override
+    protected Animal createNewPet(AnimalType type) {
+
+        switch (type) {
+            case Cat:
+                return new Cat();
+            case Dog:
+                return new Dog();
+            case Hamster:
+                return new Hamster();
+        }
+        return null;
+    }
 }
