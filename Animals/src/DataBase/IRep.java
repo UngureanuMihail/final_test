ec55dc24db885f9e270cffccbbe7c4800d576a25
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 22:58:02 +0200

    Add Class Irep

--- Animals/src/DataBase/IRep.java
+++ Animals/src/DataBase/IRep.java
@@ -1,2 +1,12 @@
-package DataBase;public class IRep {
+package DataBase;
+
+import java.util.List;
+
+public interface IRep <T> {
+
+    List <T> getAll();
+    T getById(int id);
+    int create(T item);
+    int update(T item);
+    void delete (int item);
 }
