4d4319f858beb1d0b1b30a8907438b6b6ece4918
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-03-23 23:01:00 +0200

    Добавление счетчика

--- Animals/src/Controller/Counter.java
+++ Animals/src/Controller/Counter.java
@@ -1,2 +1,18 @@
-package Controller;public class Counter {
+package Controller;
+
+public class Counter implements AutoCloseable {
+
+    static int sum;
+    {
+        sum = 0;
+    }
+
+    public void add() {
+        sum++;
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Counter closed");
+    }
 }
